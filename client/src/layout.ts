f0e5738f3ed40f169ff65e9297cdc67c70ece2e9
michaelhly <michaelhly@gmail.com>
2020-10-24 16:06:39 -0500

    Fixup

--- client/src/layout.ts
+++ client/src/layout.ts
@@ -31,12 +31,12 @@ export const rustString = (property: string = 'string'): object => {
   const _decode = rsl.decode.bind(rsl);
   const _encode = rsl.encode.bind(rsl);
 
-  rsl.decode = (buffer: typeof Buffer, offset: number) => {
+  rsl.decode = (buffer: Buffer, offset: number) => {
     const data = _decode(buffer, offset);
     return data.chars.toString('utf8');
   };
 
-  rsl.encode = (str: string, buffer: typeof Buffer, offset: number) => {
+  rsl.encode = (str: string, buffer: Buffer, offset: number) => {
     const data = {
       chars: Buffer.from(str, 'utf8'),
     };

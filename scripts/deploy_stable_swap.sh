35ecb76df3a193ab70a3d58fe5cb699183243172
michaelhly <michaelhly@gmail.com>
2020-10-26 14:20:01 -0500

    Update deployment script

--- scripts/deploy_stable_swap.sh
+++ scripts/deploy_stable_swap.sh
@@ -7,13 +7,14 @@ fi
 docker-compose up -d
 if ! hash solana 2>/dev/null; then
     echo Installing Solana tool suite ...
-    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.3.9/install/solana-install-init.sh | sh -s - v1.3.9
+    curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.3.15/install/solana-install-init.sh | sh -s - v1.3.15
     export PATH="/home/runner/.local/share/solana/install/active_release/bin:$PATH"
     echo Generating keypair ...
     solana-keygen new -o ~/.config/solana/id.json --no-passphrase --silent
 fi
-solana config set --url "http://localhost:8899"
+# solana config set --url "http://localhost:8899"
+solana config set --url "https://devnet.solana.com/"
 sleep 1
-solana airdrop 100000
+solana airdrop 10 
 STABLE_SWAP_ID="$(solana deploy target/bpfel-unknown-unknown/release/stable_swap.so | jq .programId -r)"
 echo "StableSwap ProgramID:" $STABLE_SWAP_ID

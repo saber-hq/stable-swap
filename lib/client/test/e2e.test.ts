c859ebecd5852182917188e053a968aa4417bdad
michaelhly <michaelhly@gmail.com>
2020-10-27 16:42:23 -0500

    Checkpoint commit

--- lib/client/test/e2e.test.ts
+++ lib/client/test/e2e.test.ts
@@ -4,6 +4,7 @@ import { Token } from "@solana/spl-token";
 import { Account, Connection, PublicKey } from "@solana/web3.js";
 
 import { StableSwap } from "../src";
+import { NumberU64 } from "../src/util/u64"
 import { newAccountWithLamports, sleep } from "./helpers";
 
 // Token Program
@@ -14,9 +15,11 @@ const TokenProgramId: PublicKey = new PublicKey(
 const CLUSTER_URL = "http://localhost:8899";
 const BOOTSTRAP_TIMEOUT = 300000;
 // Pool configs
-const AMP_FACTOR = 100;
-const FEE_NUMERATOR = 1;
-const FEE_DENOMINATOR = 4;
+const AMP_FACTOR = new NumberU64(100);
+const FEE_NUMERATOR = new NumberU64(1);
+const FEE_DENOMINATOR = new NumberU64(4);
+//  Other constants
+const oneSol = 1000000000;
 
 const getStableSwapAddress = (): string => {
   let data = fs.readFileSync("../../localnet-address.json", "utf-8");
@@ -37,6 +40,7 @@ describe("e2e test", () => {
   let owner: Account;
   // Token pool
   let tokenPool: Token;
+  let tokenAccountPool: PublicKey;
   // Tokens swapped
   let mintA: Token;
   let mintB: Token;
@@ -55,9 +59,8 @@ describe("e2e test", () => {
 
     console.log("Token Program ID", TokenProgramId.toString());
     console.log("StableSwap Program ID", stableSwapProgramId.toString());
-
-    payer = await newAccountWithLamports(connection, 1000000000);
-    owner = await newAccountWithLamports(connection, 1000000000);
+    payer = await newAccountWithLamports(connection, oneSol);
+    owner = await newAccountWithLamports(connection, oneSol);
     stableSwapAccount = new Account();
     try {
       [authority, nonce] = await PublicKey.findProgramAddress(
@@ -79,6 +82,13 @@ describe("e2e test", () => {
         TokenProgramId
       );
     } catch (e) {
+      console.error(e);
+    }
+
+    console.log('creating pool account');
+    try {
+      tokenAccountPool = await tokenPool.createAccount(owner.publicKey);
+    } catch(e) {
       console.error(e);
     }
 
@@ -163,12 +173,52 @@ describe("e2e test", () => {
     expect(fetchedStableSwap.stableSwap).toEqual(stableSwapAccount.publicKey);
     expect(fetchedStableSwap.tokenAccountA).toEqual(tokenAccountA);
     expect(fetchedStableSwap.tokenAccountB).toEqual(tokenAccountB);
-    // TODO: Uncomment after new spl-token client release
+    // TODO: Put mints back to swap state
     // expect(fetchedStableSwap.mintA).toEqual(mintA.publicKey);
     // expect(fetchedStableSwap.mintB).toEqual(mintB.publicKey);
-    // expect(fetchedStableSwap.poolToken).toEqual(tokenPool.publicKey);
-    expect(fetchedStableSwap.ampFactor.toNumber()).toBe(AMP_FACTOR);
-    expect(fetchedStableSwap.feeNumerator.toNumber()).toBe(FEE_NUMERATOR);
-    expect(fetchedStableSwap.feeDenominator.toNumber()).toBe(FEE_DENOMINATOR);
+    expect(fetchedStableSwap.poolToken).toEqual(tokenPool.publicKey);
+    expect(fetchedStableSwap.ampFactor).toEqual(AMP_FACTOR);
+    expect(fetchedStableSwap.feeNumerator).toEqual(FEE_NUMERATOR);
+    expect(fetchedStableSwap.feeDenominator).toEqual(FEE_DENOMINATOR);
   });
+
+  it("deposit", async () => {
+    const depositAmountA = oneSol;
+    const depositAmountB = oneSol; 
+    console.log('Creating depositor token a account');
+    const userAccountA = await mintA.createAccount(owner.publicKey);
+    await mintA.mintTo(userAccountA, owner, [], depositAmountA);
+    await mintA.approve(userAccountA, authority, owner, [], depositAmountA);
+    console.log('Creating depositor token b account');
+    const userAccountB = await mintB.createAccount(owner.publicKey);
+    await mintB.mintTo(userAccountB, owner, [], depositAmountB);
+    await mintB.approve(userAccountB, authority, owner, [], depositAmountB);
+    console.log('Creating depositor pool token account');
+    const newAccountPool = await tokenPool.createAccount(owner.publicKey);
+
+    console.log('Depositing into swap');
+    try {
+      await stableSwap.deposit(
+        userAccountA,
+        userAccountB,
+        newAccountPool,
+        depositAmountA,
+        depositAmountB,
+        0,   // To avoid slippage errors
+      );
+    } catch(e) {
+      throw new Error(e)
+    }
+
+    let info = await mintA.getAccountInfo(userAccountA);
+    expect(info.amount.toNumber()).toBe(0);
+    info = await mintB.getAccountInfo(userAccountB);
+    expect(info.amount.toNumber()).toBe(0);
+    info = await mintA.getAccountInfo(tokenAccountA);
+    expect(info.amount.toNumber()).toBe(depositAmountA);
+    info = await mintB.getAccountInfo(tokenAccountB);
+    expect(info.amount.toNumber()).toBe(depositAmountB);
+    info = await tokenPool.getAccountInfo(newAccountPool);
+    expect(info.amount.toNumber()).toBe(1);
+  })
 });

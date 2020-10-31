ba7a98b7ec4cf5e85e957928df941dc42797503c
Ian Macalinao <me@ian.pw>
2020-10-31 16:08:41 -0700

    format

--- lib/client/src/typings/solana-spl-token.d.ts
+++ lib/client/src/typings/solana-spl-token.d.ts
@@ -1,6 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+import { Token as OrigToken } from "@solana/spl-token";
+
 declare module "@solana/spl-token" {
-  import { PublicKey } from "@solana/web3.js";
-  import { Token as OrigToken } from "@solana/spl-token";
   export class Token extends OrigToken {
     publicKey: PublicKey;
   }

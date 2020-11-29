777782431c463c44468e7368af9caa50174e99a5
michaelhly <michaelhly@gmail.com>
2020-11-28 19:25:49 -0600

    Refactor pool converter fee calculation

--- src/pool_converter.rs
+++ src/pool_converter.rs
@@ -1,30 +1,55 @@
 //! Conversions for pool tokens
-use crate::bn::U256;
+use crate::{bn::U256, fees::Fees};
 
 /// Converter to determine how much to deposit / withdraw, along with
 /// proper initialization
-pub struct Converter {
+pub struct Converter<'a> {
     /// Total supply
     pub supply: U256,
     /// Token A amount
     pub token_a: U256,
     /// Token B amount
     pub token_b: U256,
+    /// Fees
+    pub fees: &'a Fees,
 }
 
-impl Converter {
+/// Encodes results of pool token shares converted to token_a or token_b
+#[derive(Debug, PartialEq)]
+pub struct ConvertResult {
+    /// Token amount after conversion
+    pub amount: U256,
+    /// Admin fee for withdraw
+    pub admin_fee: U256,
+}
+
+impl Converter<'_> {
     /// A tokens for pool tokens
-    pub fn token_a_rate(&self, pool_tokens: U256) -> Option<U256> {
-        pool_tokens
+    pub fn token_a_rate(&self, pool_tokens: U256) -> Option<ConvertResult> {
+        let amount = pool_tokens
             .checked_mul(self.token_a)?
-            .checked_div(self.supply)
+            .checked_div(self.supply)?;
+        let fee = self.fees.withdraw_fee(amount)?;
+        let admin_fee = self.fees.admin_withdraw_fee(fee)?;
+
+        Some(ConvertResult {
+            amount: amount.checked_sub(fee)?,
+            admin_fee,
+        })
     }
 
     /// B tokens for pool tokens
-    pub fn token_b_rate(&self, pool_tokens: U256) -> Option<U256> {
-        pool_tokens
+    pub fn token_b_rate(&self, pool_tokens: U256) -> Option<ConvertResult> {
+        let amount = pool_tokens
             .checked_mul(self.token_b)?
-            .checked_div(self.supply)
+            .checked_div(self.supply)?;
+        let fee = self.fees.withdraw_fee(amount)?;
+        let admin_fee = self.fees.admin_withdraw_fee(fee)?;
+
+        Some(ConvertResult {
+            amount: amount.checked_sub(fee)?,
+            admin_fee,
+        })
     }
 }
 
@@ -37,14 +62,36 @@ mod tests {
         token_b: U256,
         deposit: U256,
         supply: U256,
-        expected: Option<U256>,
+        expected_before_fees: Option<U256>,
     ) {
+        let fees = Fees {
+            admin_trade_fee_numerator: 0,
+            admin_trade_fee_denominator: 1,
+            admin_withdraw_fee_numerator: 1,
+            admin_withdraw_fee_denominator: 2,
+            trade_fee_numerator: 0,
+            trade_fee_denominator: 1,
+            withdraw_fee_numerator: 1,
+            withdraw_fee_denominator: 2,
+        };
         let calculator = Converter {
             supply,
             token_a,
             token_b,
+            fees: &fees,
         };
-        assert_eq!(calculator.token_a_rate(deposit), expected);
+        let expected_result: Option<ConvertResult> = if expected_before_fees.is_some() {
+            let expected_fees = fees.withdraw_fee(expected_before_fees.unwrap()).unwrap();
+            let expected_admin_fees = fees.admin_withdraw_fee(expected_fees).unwrap();
+            let expected_amount = expected_before_fees.unwrap() - expected_fees;
+            Some(ConvertResult {
+                amount: expected_amount,
+                admin_fee: expected_admin_fees,
+            })
+        } else {
+            None
+        };
+        assert_eq!(calculator.token_a_rate(deposit), expected_result);
         assert_eq!(calculator.supply, supply);
     }
 

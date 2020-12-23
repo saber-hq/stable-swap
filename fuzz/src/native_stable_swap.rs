d3da459fffc7cac1103b2a531af8bf4d00de1dc4
michaelhly <michaelhly@gmail.com>
2020-12-22 20:51:11 -0600

    Withdraw helper

--- fuzz/src/native_stable_swap.rs
+++ fuzz/src/native_stable_swap.rs
@@ -344,4 +344,67 @@ impl NativeStableSwap {
             ],
         )
     }
+
+    pub fn withdraw(
+        &mut self,
+        user_account: &mut NativeAccountData,
+        token_a_account: &mut NativeAccountData,
+        token_b_account: &mut NativeAccountData,
+        pool_token_account: &mut NativeAccountData,
+        instruction_data: WithdrawData,
+    ) -> ProgramResult {
+        do_process_instruction(
+            approve(
+                &self.token_program_account.key,
+                &pool_token_account.key,
+                &self.authority_account.key,
+                &user_account.key,
+                &[],
+                instruction_data.pool_token_amount,
+            )
+            .unwrap(),
+            &[
+                pool_token_account.as_account_info(),
+                self.authority_account.as_account_info(),
+                user_account.as_account_info(),
+            ],
+        )
+        .unwrap();
+
+        let withdraw_instruction = withdraw(
+            &stable_swap::id(),
+            &spl_token::id(),
+            &self.swap_account.key,
+            &self.authority_account.key,
+            &self.pool_mint_account.key,
+            &pool_token_account.key,
+            &self.token_a_account.key,
+            &self.token_b_account.key,
+            &token_a_account.key,
+            &token_b_account.key,
+            &self.admin_fee_a_account.key,
+            &self.admin_fee_a_account.key,
+            instruction_data.pool_token_amount,
+            instruction_data.minimum_token_a_amount,
+            instruction_data.minimum_token_b_amount,
+        )
+        .unwrap();
+
+        do_process_instruction(
+            withdraw_instruction,
+            &[
+                self.swap_account.as_account_info(),
+                self.authority_account.as_account_info(),
+                self.pool_mint_account.as_account_info(),
+                pool_token_account.as_account_info(),
+                self.token_a_account.as_account_info(),
+                self.token_b_account.as_account_info(),
+                token_a_account.as_account_info(),
+                token_b_account.as_account_info(),
+                self.admin_fee_a_account.as_account_info(),
+                self.admin_fee_b_account.as_account_info(),
+                self.token_program_account.as_account_info(),
+            ],
+        )
+    }
 }

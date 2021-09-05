fcb0a92880cbb0862aae5b33b5f34ee52e5aeb6b
Michael Huang <michaelhly@gmail.com>
2021-09-05 18:39:06 -0400

    Implement borsh for bn types (#70)

--- stable-swap-math/src/bn.rs
+++ stable-swap-math/src/bn.rs
@@ -4,9 +4,44 @@
 #![allow(clippy::ptr_offset_with_cast)]
 #![allow(clippy::manual_range_contains)]
 
+use borsh::{BorshDeserialize, BorshSerialize};
 use stable_swap_client::error::SwapError;
+use std::borrow::BorrowMut;
 use std::convert::TryInto;
+use std::io::{Error, ErrorKind, Write};
+use std::mem::size_of;
 use uint::construct_uint;
+
+macro_rules! impl_borsh_serialize_for_bn {
+    ($type: ident) => {
+        impl BorshSerialize for $type {
+            #[inline]
+            fn serialize<W: Write>(&self, writer: &mut W) -> std::io::Result<()> {
+                let bytes = self.to_le_bytes();
+                writer.write_all(&bytes)
+            }
+        }
+    };
+}
+
+macro_rules! impl_borsh_deserialize_for_bn {
+    ($type: ident) => {
+        impl BorshDeserialize for $type {
+            #[inline]
+            fn deserialize(buf: &mut &[u8]) -> std::io::Result<Self> {
+                if buf.len() < size_of::<$type>() {
+                    return Err(Error::new(
+                        ErrorKind::InvalidInput,
+                        "Unexpected length of input",
+                    ));
+                }
+                let res = $type::from_le_bytes(buf[..size_of::<$type>()].try_into().unwrap());
+                *buf = &buf[size_of::<$type>()..];
+                Ok(res)
+            }
+        }
+    };
+}
 
 construct_uint! {
     /// 256-bit unsigned integer.
@@ -33,7 +68,25 @@ impl U256 {
     pub fn try_to_u128(self) -> Result<u128, SwapError> {
         self.try_into().map_err(|_| SwapError::ConversionFailure)
     }
+
+    /// Convert from little endian bytes
+    pub fn from_le_bytes(bytes: [u8; 32]) -> Self {
+        U256::from_little_endian(&bytes)
+    }
+
+    /// Convert to little endian bytes
+    pub fn to_le_bytes(self) -> [u8; 32] {
+        let mut buf: Vec<u8> = Vec::with_capacity(size_of::<Self>());
+        self.to_little_endian(buf.borrow_mut());
+
+        let mut bytes: [u8; 32] = [0u8; 32];
+        bytes.copy_from_slice(buf.as_slice());
+        bytes
+    }
 }
+
+impl_borsh_deserialize_for_bn!(U256);
+impl_borsh_serialize_for_bn!(U256);
 
 construct_uint! {
     /// 192-bit unsigned integer.
@@ -60,4 +113,22 @@ impl U192 {
     pub fn try_to_u128(self) -> Result<u128, SwapError> {
         self.try_into().map_err(|_| SwapError::ConversionFailure)
     }
+
+    /// Convert from little endian bytes
+    pub fn from_le_bytes(bytes: [u8; 24]) -> Self {
+        U192::from_little_endian(&bytes)
+    }
+
+    /// Convert to little endian bytes
+    pub fn to_le_bytes(self) -> [u8; 24] {
+        let mut buf: Vec<u8> = Vec::with_capacity(size_of::<Self>());
+        self.to_little_endian(buf.borrow_mut());
+
+        let mut bytes: [u8; 24] = [0u8; 24];
+        bytes.copy_from_slice(buf.as_slice());
+        bytes
+    }
 }
+
+impl_borsh_deserialize_for_bn!(U192);
+impl_borsh_serialize_for_bn!(U192);

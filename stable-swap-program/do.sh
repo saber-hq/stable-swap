107048c7acaba10f1d49c58e6cdbce2acc86a36f
Ian Macalinao <ian@saber.so>
2021-12-28 12:21:29 -0800

    Include all members, but alter build

--- stable-swap-program/do.sh
+++ stable-swap-program/do.sh
@@ -19,7 +19,7 @@ perform_action() {
     e2e-test)
         (
             rm -rf scripts/tmp
-            anchor build
+            anchor build --program-name stable_swap
             solana-test-validator --quiet --bpf-program SSwpkEEcbUqx4vtoEByFjSkhKdCT862DNVb52nZg1UZ ../target/deploy/stable_swap.so &
             yarn --cwd sdk install
             # Possible race condition here if the validator isn't up

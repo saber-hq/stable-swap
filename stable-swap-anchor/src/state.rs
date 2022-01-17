2906263b8c7c4ec12132ad1773346b4b02418bf4
Ian Macalinao <ian@saber.so>
2022-01-17 17:32:42 -0500

    Clean up Anchor SDK (#115)
    * Allow passing in an 'AccountInfo' to AdminUserContext
    
    * Clean up Anchor files
    
    * Fix doc generation
    
    * Fix docs, ensure user passed in becomes signer
    
    * remove unnecessary clock in Anchor
    
    * More docs cleanup

---
+++ stable-swap-anchor/src/state.rs
@@ -0,0 +1,52 @@
+//! State structs for swaps.
+
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::program_pack::Pack;
+use std::ops::Deref;
+
+/// StableSwap account wrapper for Anchor programs.
+///
+/// *For more info, see [stable_swap_client::state::SwapInfo].*
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct SwapInfo(stable_swap_client::state::SwapInfo);
+
+impl SwapInfo {
+    /// The length, in bytes, of the packed representation
+    pub const LEN: usize = stable_swap_client::state::SwapInfo::LEN;
+
+    /// Computes the minimum rent exempt balance of a [SwapInfo].
+    pub fn minimum_rent_exempt_balance() -> Result<u64, ProgramError> {
+        Ok(Rent::get()?.minimum_balance(Self::LEN))
+    }
+}
+
+impl Owner for SwapInfo {
+    fn owner() -> Pubkey {
+        crate::ID
+    }
+}
+
+impl Deref for SwapInfo {
+    type Target = stable_swap_client::state::SwapInfo;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl anchor_lang::AccountSerialize for SwapInfo {
+    fn try_serialize<W: std::io::Write>(&self, _writer: &mut W) -> Result<(), ProgramError> {
+        // no-op
+        Ok(())
+    }
+}
+
+impl anchor_lang::AccountDeserialize for SwapInfo {
+    fn try_deserialize(buf: &mut &[u8]) -> Result<Self, ProgramError> {
+        SwapInfo::try_deserialize_unchecked(buf)
+    }
+
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> Result<Self, ProgramError> {
+        stable_swap_client::state::SwapInfo::unpack(buf).map(SwapInfo)
+    }
+}
